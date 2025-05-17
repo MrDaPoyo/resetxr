d678c09228b5277a88e5ebfa9c79e80cdbd874a6
MrDaPoyo <wiiuforever25@gmail.com>
2025-05-17 18:39:26 +0200

    info about games when hovered

--- src/components/scrolling_games.svelte
+++ src/components/scrolling_games.svelte
@@ -13,6 +13,17 @@
 						src="/library_600x900.jpg"
 						draggable="false"
 					/>
+					<div class="backtile">
+						<h3>Balatro</h3>
+						<p>
+							Balatro es un juego de cartas único que combina
+							estrategia y diversión.<br />¡Prueba tu suerte
+							ahora!<small
+								>No nos responsabilizamos de cualquier perdida
+								de dinero ocasionada por las apuestas.</small
+							>
+						</p>
+					</div>
 				</div>
 			{/each}
 		</div>
@@ -25,6 +36,17 @@
 						src="/library_600x900.jpg"
 						draggable="false"
 					/>
+					<div class="backtile">
+						<h3>Balatro</h3>
+						<p>
+							Balatro es un juego de cartas único que combina
+							estrategia y diversión.<br />¡Prueba tu suerte
+							ahora!<small
+								>No nos responsabilizamos de cualquier perdida
+								de dinero ocasionada por las apuestas.</small
+							>
+						</p>
+					</div>
 				</div>
 			{/each}
 		</div>
@@ -37,9 +59,9 @@
 		width: 100%;
 		height: fit-content;
 		overflow: hidden;
-        max-height: 70vh;
+		max-height: 70vh;
 	}
-    
+
 	.marquee-content {
 		display: flex;
 		width: 100%;
@@ -52,7 +74,7 @@
 		justify-content: center;
 		will-change: transform opacity filter;
 		animation-play-state: running;
-    }
+	}
 
 	.left .marquee-content {
 		animation-name: scroll-left;
@@ -70,7 +92,7 @@
 	.down .marquee-content {
 		animation-name: scroll-down;
 		flex-direction: column;
-    }
+	}
 
 	.marquee-content:hover {
 		animation-play-state: paused;
@@ -81,32 +103,86 @@
 		flex-direction: row;
 		align-items: center;
 	}
-	
-	.up .strip, .down .strip {
+
+	.up .strip,
+	.down .strip {
 		flex-direction: column;
 		margin-bottom: 20px;
 	}
 
 	.tile {
 		height: auto;
-        position: relative;
+		position: relative;
 		width: 100%;
-        display: flex;
-        flex-grow: 1;
+		display: flex;
+		flex-grow: 1;
 		margin: 10px;
 		box-shadow: 0 4px 10px rgba(0, 0, 0, 0.4);
-	    transition: 0.1s ease-in-out;
-        filter: grayscale(100%);
-        border-radius: 25px;
-        overflow: hidden;
+		transition: 0.1s ease-in-out;
+		filter: grayscale(100%);
+		border-radius: 25px;
+		overflow: hidden;
 		will-change: transform opacity filter;
 	}
 
-    .tile:hover {
-        filter: grayscale(0%);
+	.backtile {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		color: white;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: auto;
+		font-size: 1rem;
+		backdrop-filter: blur(10px);
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+		z-index: 100;
+	}
+
+	.backtile h3 {
+		margin: 0;
+		font-size: 1.5rem;
+	}
+
+	.tile:hover {
+		.backtile {
+			opacity: 1;
+		}
+	}
+
+	.tile img {
+		width: 100%;
+		height: auto;
+		display: block;
+	}
+
+	.tile::after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(255, 255, 255, 0.5);
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+	.tile:hover::after {
+		opacity: 1;
+	}
+
+	.tile:hover {
+		filter: grayscale(0%);
 		transform: scale(1.05);
 		display: block;
-    }
+	}
 
 	@keyframes scroll-left {
 		0% {
@@ -125,21 +201,21 @@
 			transform: translateX(0);
 		}
 	}
-    @keyframes scroll-up {
-        0% {
-            transform: translateY(0);
-        }
-        100% {
-            transform: translateY(calc(-50% - 1px));
-        }
-    }
+	@keyframes scroll-up {
+		0% {
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateY(calc(-50% - 1px));
+		}
+	}
 
-    @keyframes scroll-down {
-        0% {
-            transform: translateY(calc(-50% - 1px));
-        }
-        100% {
-            transform: translateY(0);
-        }
-    }
+	@keyframes scroll-down {
+		0% {
+			transform: translateY(calc(-50% - 1px));
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
 </style>
